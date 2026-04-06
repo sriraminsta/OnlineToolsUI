1be987c5331e7828e1f61b23c469f0a3eecc8636
sriraminsta <sriram.sakinala@outlook.com>
2026-04-05 22:04:39 -0400

    added types and data folders

---
+++ src/types/Tool.ts
@@ -0,0 +1,12 @@
+type ToolType = 'built' | 'popular' | 'soon';
+
+export interface Tool {
+    n: string; // name
+    d: string; // description
+    i: string; // icon (emoji)
+    b: string; // background color
+    t: ToolType; // type/category
+    u: string; // url
+    SearchTerms: string[];
+    id:number
+}
